b3a426077de9c2d306a71d8e44c749da9120e875
lamn50478 <lamn50478@gmail.com>
2026-04-05 22:30:03 +0530

    index

--- index.js
+++ index.js
@@ -1,82 +1,20 @@
-// const express=require('express')
-// const app=express()
-// var flash=require('express-flash')
-// const path = require('path');
-
-
-// const systemConfig=require('./config/system.js')
-
-// const routerAdmin=require('./routers/admin/index.route.js')
-// const router= require('./routers/client/route_index')
-// const methodOverride=require("method-override")
-// const bodyParser= require("body-parser");
-// const cookieParser=require("cookie-parser");
-// const expressSession=require("express-session");
-// const moment=require("moment");
-
-// const database=require("./config/database");
-// //env set
-// require('dotenv').config();
-// const port=process.env.PORT || 3000;
-// //end set env
-
-// //flash
-// app.use(cookieParser("12345"));
-// app.use(expressSession({cookie : {maxAge:60000}}));
-// app.use(flash());
-// //end flash
-
-// //tiny mce
-// app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
-// //end tiny mce
-
-// //body-parse
-// app.use(bodyParser.urlencoded({extended:true}))
-// app.use(express.json());
-// //end body-parse
-
-// //method 
-// app.use(methodOverride("_method"));
-// //end method
-// app.locals.prefixAdmin=systemConfig.prefixAdmin;
-// app.locals.moment=moment;
-// database.connect();
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.set('views', path.join(__dirname, 'views'));
-// // app.set("views",`${__dirname}/views`)  //cm
-// app.set("view engine","pug")
-// // app.use(express.static(`${__dirname}/public`))
-
-// //route
-// router(app)
-// routerAdmin(app)
-
-
-// app.listen(port,()=>{
-//     console.log(`example listening on ${port}`);
-// })
-// // module.exports=app
-// // 
-
-
-//---------------------------------------------
-
 const express=require('express')
 const app=express()
 var flash=require('express-flash')
 const path = require('path');
 
+
 const systemConfig=require('./config/system.js')
+
 const routerAdmin=require('./routers/admin/index.route.js')
 const router= require('./routers/client/route_index')
 const methodOverride=require("method-override")
 const bodyParser= require("body-parser");
 const cookieParser=require("cookie-parser");
 const expressSession=require("express-session");
-const MongoStore=require("connect-mongo");
 const moment=require("moment");
-const database=require("./config/database");
 
+const database=require("./config/database");
 //env set
 require('dotenv').config();
 const port=process.env.PORT || 3000;
@@ -84,15 +22,7 @@ const port=process.env.PORT || 3000;
 
 //flash
 app.use(cookieParser("12345"));
-app.use(expressSession({
-    secret:"12345",
-    resave:false,
-    saveUninitialized:false,
-    store: process.env.MONGO_URL
-        ? MongoStore.create({ mongoUrl: process.env.MONGO_URL, ttl: 60*60 })
-        : undefined,
-    cookie:{maxAge:60000}
-}));
+app.use(expressSession({cookie : {maxAge:60000}}));
 app.use(flash());
 //end flash
 
@@ -105,26 +35,29 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json());
 //end body-parse
 
-//method
+//method 
 app.use(methodOverride("_method"));
 //end method
-
 app.locals.prefixAdmin=systemConfig.prefixAdmin;
 app.locals.moment=moment;
-
-database.connect().catch(err => console.error('❌ DB Error:', err));
-
+database.connect();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
+// app.set("views",`${__dirname}/views`)  //cm
 app.set("view engine","pug")
+// app.use(express.static(`${__dirname}/public`))
 
 //route
 router(app)
 routerAdmin(app)
 
-if(require.main === module){
-    app.listen(port,()=>{
-        console.log(`example listening on ${port}`);
-    });
-}
-module.exports=app;
+
+app.listen(port,()=>{
+    console.log(`example listening on ${port}`);
+})
+// module.exports=app
+// 
+
+
+//---------------------------------------------
+
